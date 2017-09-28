f3f160d61d19ab3fdf598978cc39d8bd0d1a0b58
Jurbin <jurbin6090@gmail.com>
2017-09-28 10:17:10 -0500

    Updated login controller

--- src/main/java/com/weight/controller/LoginController.java
+++ src/main/java/com/weight/controller/LoginController.java
@@ -2,8 +2,17 @@ package com.weight.controller;
 
 import com.weight.entity.Login;
 import com.weight.repository.LoginRepository;
+import org.apache.commons.crypto.random.CryptoRandom;
+import org.apache.commons.crypto.random.CryptoRandomFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
 
 @RestController
 @RequestMapping("/login")
@@ -11,28 +20,61 @@ public class LoginController {
 
     @Autowired
     private LoginRepository repository;
+    byte[] salt = new byte[32];
 
     @CrossOrigin(origins = "*")
     @PostMapping
-    public boolean login(@RequestBody Login login){
-        String salt = getSalt(login.getEmail());
+    public boolean login(@RequestBody Login login) {
+        Login retrievedLogin = repository.findByEmail(login.getEmail());
+        String hashedPassword = md5Hash(login.getPassword(), retrievedLogin.getSalt());
 
-        if(login.getEmail().equals("jurbin6090@gmail.com") && login.getPassword().equals(salt + "Fedex"))
-           return true;
+        if (login.getEmail().equals("jurbin6090@gmail.com") && retrievedLogin.getPassword().equals(hashedPassword))
+            return true;
         return false;
     }
 
     @CrossOrigin(origins = "*")
-    @PostMapping("/getSalt")
-    public String getSalt(@RequestBody String email){
-        Login login = repository.findByEmail(email);
-        return login.getSalt();
+    @PutMapping
+    public String addLogin(@RequestBody Login login) {
+        random();
+
+        String hashedPassword = md5Hash(login.getPassword(), salt.toString());
+
+        login.setSalt(salt.toString());
+        login.setPassword(hashedPassword);
+
+        repository.save(login);
+        return "Login saved " + hashedPassword;
     }
 
-    @CrossOrigin(origins = "*")
-    @PutMapping
-    public String addLogin(@RequestBody Login login){
-        repository.save(login);
-        return "Login saved";
+    private void random() {
+        Properties properties = new Properties();
+        properties.put(CryptoRandomFactory.CLASSES_KEY, CryptoRandomFactory.RandomProvider.OPENSSL.getClassName());
+
+        try (CryptoRandom random = CryptoRandomFactory.getCryptoRandom(properties)) {
+            random.nextBytes(salt);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static String md5Hash(String message, String salt) {
+        String md5 = "";
+        if (null == message)
+            return null;
+
+        message = message + salt;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(message.getBytes(), 0, message.length());
+            md5 = new BigInteger(1, digest.digest()).toString(16);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return md5;
+    }
+
 }
